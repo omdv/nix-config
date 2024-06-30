cc7a4af4f6fb6bf10e98996f893353d3d35aae8d
Oleg Medvedev <omdv@protonmail.com>
2024-06-30 09:41:17 -0500

    Initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,134 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./firefox.nix
+    # ./kitty.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  home = {
+    username = "om";
+    homeDirectory = "/home/om";
+  };
+
+  # Console settings 
+  programs.atuin = {
+     enable = true;
+     enableFishIntegration = true;
+  };
+  programs.fish.enable = true;
+
+  programs.kitty = {
+  	enable = true;
+  	theme = "Tokyo Night";
+  	font.name = "JetBrainsMono Nerd Font";
+	shellIntegration.enableFishIntegration = true;
+  	settings = {
+  		confirm_os_window_close = -0;
+  		copy_on_select = true;
+  		clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
+  	};
+  };
+
+
+  programs.firefox.enable = true;
+  programs.vscode.enable = true;
+  programs.neovim.enable = true;
+  # home.packages = with pkgs; [ steam ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+
+  # Gnupg
+  services.gnome-keyring.enable = true;
+
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+	enable = true;
+	pinentryFlavor = "qt";
+  }; 
+
+  # Git config
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+    aliases = {
+      p = "pull --ff-only";
+      ff = "merge --ff-only";
+      graph = "log --decorate --oneline --graph";
+      pushall = "!git remote | xargs -L1 git push --all";
+      add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+    };
+    userName = "Oleg Medvedev";
+    userEmail = lib.mkDefault "omdv@protonmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      user.signing.key = "C86CD9E2DCEB2452 ";
+      commit.gpgSign = lib.mkDefault true;
+      gpg.program = "${config.programs.gpg.package}/bin/gpg2";
+
+      merge.conflictStyle = "zdiff3";
+      commit.verbose = true;
+      diff.algorithm = "histogram";
+      log.date = "iso";
+      column.ui = "auto";
+      branch.sort = "committerdate";
+      # Automatically track remote branch
+      push.autoSetupRemote = true;
+      # Reuse merge conflict fixes when rebasing
+      rerere.enabled = true;
+    };
+    lfs.enable = true;
+    ignores = [
+      ".direnv"
+      "result"
+    ];
+  };
+
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
