2458fbfeec70914f484fbd7e885d09a51e554f60
Oleg Medvedev <omdv@protonmail.com>
2024-06-30 10:49:01 -0500

    vscode config

---
+++ home-manager/vscode.nix
@@ -0,0 +1,36 @@
+{pkgs, ...}: {
+  programs.vscode = {
+    enable = true;
+    userSettings = {
+      # General
+      "editor.fontSize" = 16;
+      "editor.fontFamily" = "'Jetbrains Mono', 'monospace', monospace";
+      "terminal.integrated.fontSize" = 14;
+      "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font', 'monospace', monospace";
+      "window.zoomLevel" = 1;
+      "editor.multiCursorModifier" = "ctrlCmd";
+      "workbench.startupEditor" = "none";
+      "explorer.compactFolders" = false;
+      # Whitespace
+      "files.trimTrailingWhitespace" = true;
+      "files.trimFinalNewlines" = true;
+      "files.insertFinalNewline" = true;
+      "diffEditor.ignoreTrimWhitespace" = false;
+      # Git
+      "git.enableCommitSigning" = true;
+      "git-graph.repository.sign.commits" = true;
+      "git-graph.repository.sign.tags" = true;
+      "git-graph.repository.commits.showSignatureStatus" = true;
+      # Styling
+      "window.autoDetectColorScheme" = true;
+      "workbench.preferredDarkColorTheme" = "Default Dark Modern";
+      "workbench.preferredLightColorTheme" = "Default Light Modern";
+      "workbench.iconTheme" = "material-icon-theme";
+      "material-icon-theme.activeIconPack" = "none";
+      "material-icon-theme.folders.theme" = "classic";
+      # Other
+      "telemetry.telemetryLevel" = "off";
+      "update.showReleaseNotes" = false;
+    };
+  };
+}
