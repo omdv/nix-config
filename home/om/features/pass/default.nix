a9273b3761e99d6e60e8a6b7f316ef7cbbcd585f
Oleg Medvedev <omdv@protonmail.com>
2024-06-30 22:22:24 -0500

    pass in hm

--- home/om/features/pass/default.nix
+++ home/om/features/pass/default.nix
@@ -10,14 +10,4 @@
     };
     package = pkgs.pass.withExtensions (p: [p.pass-otp]);
   };
-
-  services.pass-secret-service = {
-    enable = true;
-    storePath = "${config.home.homeDirectory}/.password-store";
-    extraArgs = ["-e${config.programs.password-store.package}/bin/pass"];
-  };
-
-  home.persistence = {
-    "/persist/${config.home.homeDirectory}".directories = [".password-store"];
-  };
 }
