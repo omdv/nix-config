d0e88b7a291a1b16f5896dd99e6b369592cb91b7
Oleg Medvedev <omdv@protonmail.com>
2024-07-02 17:47:42 -0500

    more home tools

--- home/om/features/cli/default.nix
+++ home/om/features/cli/default.nix
@@ -13,15 +13,25 @@
     distrobox # Nice escape hatch, integrates docker images with my environment
 
     # bc # Calculator
-    # bottom # System viewer
-    ncdu # TUI disk usage
+
+    bottom # System viewer
+    htop # System viewer
+    glances # System viewer
+
+    sysz # fzf for systemd
+
+    bat # Better cat
     eza # Better ls
     ripgrep # Better grep
     fd # Better find
     httpie # Better curl
     diffsitter # Better diff
+
+    ncdu # TUI disk usage
+    duf # TUI disk usage
+
     jq # JSON pretty printer and manipulator
-    # trekscii # Cute startrek cli printer
+    trekscii # Cute startrek cli printer
 
     # nixd # Nix LSP
     # alejandra # Nix formatter
