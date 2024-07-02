a8a61ff5d6839f8a12d4c9b5b8bb959bcd316e6f
Oleg Medvedev <omdv@protonmail.com>
2024-07-02 17:33:36 -0500

    home programs

--- home/om/features/cli/default.nix
+++ home/om/features/cli/default.nix
@@ -9,28 +9,28 @@
     ./starship.nix
   ];
   home.packages = with pkgs; [
-#    comma # Install and run programs by sticking a , before them
-#    distrobox # Nice escape hatch, integrates docker images with my environment
+    comma # Install and run programs by sticking a , before them
+    distrobox # Nice escape hatch, integrates docker images with my environment
 
-#    bc # Calculator
-#    bottom # System viewer
-#    ncdu # TUI disk usage
-#    eza # Better ls
-#    ripgrep # Better grep
-#    fd # Better find
-#    httpie # Better curl
-#    diffsitter # Better diff
+    # bc # Calculator
+    # bottom # System viewer
+    ncdu # TUI disk usage
+    eza # Better ls
+    ripgrep # Better grep
+    fd # Better find
+    httpie # Better curl
+    diffsitter # Better diff
     jq # JSON pretty printer and manipulator
-#    trekscii # Cute startrek cli printer
+    # trekscii # Cute startrek cli printer
 
-#    nixd # Nix LSP
-#    alejandra # Nix formatter
-#    nixfmt-rfc-style
-#    nvd # Differ
-#    nix-diff # Differ, more detailed
-#    nix-output-monitor
-#    nh # Nice wrapper for NixOS and HM
+    # nixd # Nix LSP
+    # alejandra # Nix formatter
+    # nixfmt-rfc-style
+    # nvd # Differ
+    # nix-diff # Differ, more detailed
+    # nix-output-monitor
+    # nh # Nice wrapper for NixOS and HM
 
-#    ltex-ls # Spell checking LSP
+    # ltex-ls # Spell checking LSP
   ];
 }
