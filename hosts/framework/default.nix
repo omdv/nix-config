6c7659a98c56899112fcb650d0c4c066483611f0
Oleg Medvedev <omdv@protonmail.com>
2024-07-02 20:01:34 -0500

    borgmatic and btrfs scrub

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -14,6 +14,7 @@
     ../common/optional/docker.nix
     ../common/optional/pipewire.nix
     ../common/optional/java.nix
+    ../common/optional/btrfs.nix
   ];
 
   programs = {
@@ -39,6 +40,44 @@
       "aarch64-linux"
       "i686-linux"
     ];
+  };
+
+  # borgmatic
+  services.borgmatic = {
+    enable = true;
+    config = {
+      location = "/etc/borgmatic";
+      config = ''
+        location:
+          source_directories:
+            - /home/om/
+          repositories:
+            - ssh://ylc5axw6@ylc5axw6.repo.borgbase.com/./repo
+          remote_path: borg1
+          exclude_patterns:
+            - '*.pyc'
+            - /home/om/.cache
+            - /home/om/.ssh
+            - /home/om/.config/borg
+            - '/home/om/*/node_modules/'
+            - '/home/om/*/Trash/'
+            - '/home/om/*/.venv/'
+            - /home/om/Jts
+            - /home/om/.asdf
+            - /home/om/.gnupg
+          exclude_if_present:
+            - .nobackup
+          exclude_caches: true
+          storage:
+            encryption_passcommand: pass show backup/borgbase-arch
+          retention:
+            keep_daily: 7
+            keep_weekly: 4
+            keep_monthly: 2
+          ssh_command: ssh -i /home/om/.ssh/id_rsa
+          borg_base_directory: /home/om/
+      '';
+    };
   };
 
   system.stateVersion = "23.05";
