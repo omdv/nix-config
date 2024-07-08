ec9f00e6fd0a2e6cf9918d1ea6108d99955fca42
Oleg Medvedev <omdv@protonmail.com>
2024-07-08 14:33:33 -0500

    pass folder settings

--- home/om/features/cli/kitty.nix
+++ home/om/features/cli/kitty.nix
@@ -8,9 +8,12 @@
     };
     shellIntegration.enableFishIntegration = true;
     settings = {
+      scrollback_lines = 4000;
+      scrollback_pager_history_size = 100000;
       confirm_os_window_close = -0;
       copy_on_select = true;
       clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
+
       # for nnn
       allow_remote_control = "yes";
       listen_on = "unix:/tmp/kitty";
