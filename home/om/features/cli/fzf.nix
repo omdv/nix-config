5bbc7c97c1d5344654b08513152bd0b3b629d64b
Oleg Medvedev <omdv@protonmail.com>
2024-07-08 20:43:26 -0500

    fzf default settings

--- home/om/features/cli/fzf.nix
+++ home/om/features/cli/fzf.nix
@@ -1,6 +1,13 @@
-{
+{ pkgs, ... }: {
   programs.fzf = {
     enable = true;
-    defaultOptions = ["--color 16"];
+    enableFishIntegration = true;
+    # defaultOptions reloads only with restart
+    defaultOptions = [
+      "--inline-info"
+      "--color 16"
+      "--height 100%"
+      "--preview 'bat --color=always {}'"
+    ];
   };
 }
