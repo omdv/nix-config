ed00b0ae328ae944a991a895d8639db09fe18bb3
Oleg Medvedev <omdv@protonmail.com>
2024-07-12 18:56:19 -0500

    additional packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,19 @@
 # Build them using 'nix build .#example'
-{pkgs, ...}:
-{
+{pkgs ? import <nixpkgs> {}, ...}: rec {
+  # Packages with an actual source
   myjdk = pkgs.callPackage ./myjdk { };
-  pass-wofi = pkgs.callPackage ./pass-wofi { };
+  shellcolord = pkgs.callPackage ./shellcolord {};
+
+  # My wallpaper collection
+  wallpapers = import ./wallpapers {inherit pkgs;};
+  allWallpapers = pkgs.linkFarmFromDrvs "wallpapers" (pkgs.lib.attrValues wallpapers);
+
+  # And colorschemes based on it
+  generateColorscheme = import ./colorschemes/generator.nix {inherit pkgs;};
+  colorschemes = import ./colorschemes {inherit pkgs wallpapers generateColorscheme;};
+  allColorschemes = let
+    # This is here to help us keep IFD cached (hopefully)
+    combined = pkgs.writeText "colorschemes.json" (builtins.toJSON (pkgs.lib.mapAttrs (_: drv: drv.imported) colorschemes));
+  in
+    pkgs.linkFarmFromDrvs "colorschemes" (pkgs.lib.attrValues colorschemes ++ [combined]);
 }
