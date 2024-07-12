1d7e1383d74fc03180dbc31137058a931d9a6954
Oleg Medvedev <omdv@protonmail.com>
2024-07-12 18:48:49 -0500

    additional modules

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,8 +1,7 @@
-# Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
-# These should be stuff you would like to share with others, not your personal configurations.
 {
-  # List your module files here
   fonts = import ./fonts.nix;
   monitors = import ./monitors.nix;
-  # pass-secret-service = import ./pass-secret-service.nix;
+  shellcolor = import ./shellcolor.nix;
+  wallpaper = import ./wallpaper.nix;
+  colors = import ./colors.nix;
 }
