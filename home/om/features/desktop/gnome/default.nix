8f876358f27a2ff70aec059f9490d5336912d3bd
Oleg Medvedev <omdv@protonmail.com>
2024-07-16 17:52:09 -0500

    stable state for gnome

--- home/om/features/desktop/gnome/default.nix
+++ home/om/features/desktop/gnome/default.nix
@@ -3,6 +3,6 @@
   ...
 }: {
   # Scaling in GNOME
-  dconf.settings."org/gnome/desktop/interface".text-scaling-factor = 1.28;
+  dconf.settings."org/gnome/desktop/interface".text-scaling-factor = 1.3333;
   dconf.settings."org/gnome/desktop/background".picture-uri-dark = config.wallpaper.url;
 }
