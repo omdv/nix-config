09a4868c5955a0866466a175f10f86568150c2d2
Oleg Medvedev <omdv@protonmail.com>
2024-07-16 19:33:52 -0500

    initial i3wm config

---
+++ home/om/features/desktop/i3wm/keybindings.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, config, ... }: {
+  xsession.windowManager.i3 = {
+    config = {
+      modifier = "Mod4";
+      keybindings = let
+        mod = config.xsession.windowManager.i3.config.modifier;
+      in lib.mkOptionDefault {
+        "${mod}+Return" = "exec ${lib.getExe config.programs.kitty.package}";
+        "${mod}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+      };
+    };
+  };
+}
