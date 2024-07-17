fb4713cf4b41929685555433a6988b4dae7ae9d3
Oleg Medvedev <omdv@protonmail.com>
2024-07-16 22:46:26 -0500

    color scheme

--- home/om/features/desktop/i3wm/default.nix
+++ home/om/features/desktop/i3wm/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: {
+{ pkgs, config, ... }: {
   imports = [
     ./keybindings.nix
     ./statusbar.nix
@@ -8,6 +8,17 @@
     enable = true;
     package = pkgs.i3-gaps;
     config = {
+
+      fonts = {
+        names = [ config.fontProfiles.monospace.family ];
+        style = "Mono";
+        size = 14.0;
+      };
+
+      gaps = {
+        inner = 2;
+        top = 25;
+      };
     };
   };
 }
