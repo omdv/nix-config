d7352b6a3877804539d42244e34637f7775a40aa
Oleg Medvedev <omdv@protonmail.com>
2024-07-16 19:33:52 -0500

    initial i3wm config

--- home/om/features/desktop/i3wm/default.nix
+++ home/om/features/desktop/i3wm/default.nix
@@ -1,13 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
+  imports = [
+    ./keybindings.nix
+    ./statusbar.nix
+  ];
+
   xsession.windowManager.i3 = {
     enable = true;
     package = pkgs.i3-gaps;
     config = {
-      modifier = "Mod4";
-      gaps = {
-        inner = 10;
-        outer = 5;
-      };
     };
   };
 }
---
+++ home/om/features/desktop/i3wm/statusbar.nix
@@ -0,0 +1,16 @@
+{
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      top = {
+        blocks = [
+          {
+            block = "time";
+            interval = 60;
+            format = "%a %d/%m %k:%M %p";
+          }
+        ];
+      };
+    };
+  };
+}
