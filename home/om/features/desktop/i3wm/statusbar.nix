539f73e6d44aeccc7a15f28666cd8d08d8f68593
Oleg Medvedev <omdv@protonmail.com>
2024-07-17 21:12:25 -0500

    email status

--- home/om/features/desktop/i3wm/statusbar.nix
+++ home/om/features/desktop/i3wm/statusbar.nix
@@ -1,4 +1,46 @@
-{ pkgs, lib, config, ... }: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  commonDeps = with pkgs; [coreutils gnugrep systemd];
+  # Function to simplify making waybar outputs
+  mkScript = {
+    name ? "script",
+    deps ? [],
+    script ? "",
+  }:
+    lib.getExe (pkgs.writeShellApplication {
+      inherit name;
+      text = script;
+      runtimeInputs = commonDeps ++ deps;
+    });
+  # Specialized for JSON outputs
+  mkScriptJson = {
+    name ? "script",
+    deps ? [],
+    pre ? "",
+    text ? "",
+    tooltip ? "",
+    alt ? "",
+    class ? "",
+    percentage ? "",
+  }:
+    mkScript {
+      deps = [pkgs.jq] ++ deps;
+      script = ''
+        ${pre}
+        jq -cn \
+          --arg text "${text}" \
+          --arg tooltip "${tooltip}" \
+          --arg alt "${alt}" \
+          --arg class "${class}" \
+          --arg percentage "${percentage}" \
+          '{text:$text,tooltip:$tooltip,alt:$alt,class:$class,percentage:$percentage}'
+      '';
+    };
+in {
   xsession.windowManager.i3 = {
     config = {
       bars = [
@@ -28,6 +70,28 @@
           {
             block = "cpu";
             interval = 1;
+          }
+          {
+            block = "custom";
+            interval = 10;
+            json = true;
+            command = mkScriptJson {
+              deps = [pkgs.findutils pkgs.procps];
+              pre = ''
+                count=$(find ~/Mail/*/Inbox/new -type f | wc -l)
+                if pgrep mbsync &>/dev/null; then
+                  status="syncing"
+                else
+                  if [ "$count" == "0" ]; then
+                    status="read"
+                  else
+                    status="unread"
+                  fi
+                fi
+              '';
+              text = "󰇮  $count";
+              alt = "$status";
+            };
           }
           {
             block = "sound";
