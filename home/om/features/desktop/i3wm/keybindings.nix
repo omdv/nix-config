e672067073ae156860223f1fd355420f7e586579
Oleg Medvedev <omdv@protonmail.com>
2024-07-22 19:50:16 -0500

    custom volume status

--- home/om/features/desktop/i3wm/keybindings.nix
+++ home/om/features/desktop/i3wm/keybindings.nix
@@ -7,6 +7,13 @@
       in lib.mkOptionDefault {
         "${mod}+Return" = "exec ${lib.getExe config.programs.kitty.package}";
         "${mod}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
+        # volume
+# # gradular volume control
+# bindsym $mod+XF86AudioRaiseVolume exec amixer -D pulse sset Master 1%+ && pkill -RTMIN+1 polybar
+# bindsym $mod+XF86AudioLowerVolume exec amixer -D pulse sset Master 1%- && pkill -RTMIN+1 polybar
+
+# # mute
+# bindsym XF86AudioMute exec amixer sset Master toggle && killall -USR1 polybar
       };
     };
   };
