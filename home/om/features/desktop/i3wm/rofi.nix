68cafbd289426f2718538fc2dc80eaf4358d652f
Oleg Medvedev <omdv@protonmail.com>
2024-07-29 17:42:49 -0500

    new rofi style

--- home/om/features/desktop/i3wm/rofi.nix
+++ home/om/features/desktop/i3wm/rofi.nix
@@ -3,6 +3,11 @@
   programs.rofi = {
     enable = true;
     terminal = lib.getExe config.programs.kitty.package;
-    theme = "glue_pro_blue";
+    # theme = "glue_pro_blue";
+    theme = "${config.xdg.configHome}/rofi/theme.rasi";
+    font = "${config.fontProfiles.monospace.family} 14";
   };
+
+  xdg.configFile."rofi/theme.rasi".source = ./rofi/theme.rasi;
+  xdg.configFile."rofi/colors.rasi".source = ./rofi/colors.rasi;
 }
