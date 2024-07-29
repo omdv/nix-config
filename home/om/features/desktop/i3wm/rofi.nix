525649642f90bda180944b8105c9a67843aeb4d0
Oleg Medvedev <omdv@protonmail.com>
2024-07-29 17:59:36 -0500

    riced rofi

--- home/om/features/desktop/i3wm/rofi.nix
+++ home/om/features/desktop/i3wm/rofi.nix
@@ -1,13 +1,22 @@
-# TODO rice rofi
-{ config, lib, ... }: {
+{ config, lib, ... }: let
+  inherit (config.colorscheme) colors;
+in {
   programs.rofi = {
     enable = true;
     terminal = lib.getExe config.programs.kitty.package;
-    # theme = "glue_pro_blue";
     theme = "${config.xdg.configHome}/rofi/theme.rasi";
-    font = "${config.fontProfiles.monospace.family} 14";
+    font = "${config.fontProfiles.monospace.family} 16";
   };
 
-  xdg.configFile."rofi/theme.rasi".source = ./rofi/theme.rasi;
-  xdg.configFile."rofi/colors.rasi".source = ./rofi/colors.rasi;
+  xdg.configFile."rofi/theme.rasi".source = ./rofi-theme.rasi;
+  xdg.configFile."rofi/colors.rasi".text = ''
+  * {
+      background: ${colors.surface_container_high};
+      background-alt: ${colors.surface_container_highest};
+      foreground: ${colors.primary};
+      selected: ${colors.on_primary_container};
+      active: ${colors.inverse_surface};
+      urgent: ${colors.error};
+  }
+  '';
 }
