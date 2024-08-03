bb26aecc63524e5c1e80fee18fb7f97293d667fc
Oleg Medvedev <omdv@protonmail.com>
2024-08-02 20:29:47 -0500

    adding pre-commit hooks

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{pkgs ? import <nixpkgs> {}, ...}: {
+  default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
+    nativeBuildInputs = with pkgs; [
+      git
+      pre-commit
+    ];
+  };
+}
