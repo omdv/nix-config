6718b61426fb776cf942f30b2a8c61d10ef1b31a
Oleg Medvedev <omdv@protonmail.com>
2024-08-05 21:29:56 -0500

    tmux settings and removing secrets pre-commit hook

--- home/om/features/cli/tmux.nix
+++ home/om/features/cli/tmux.nix
@@ -2,8 +2,10 @@
   programs.tmux = {
     enable = true;
     package = pkgs.tmux;
+    prefix = "C-a";
     aggressiveResize = true;
     clock24 = true;
     shell = "${pkgs.fish}/bin/fish";
+    sensibleOnTop = true;
   };
 }
