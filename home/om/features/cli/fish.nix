8216e6ecbb00cbde7f665dcad0b59ba92dc2c40b
Oleg Medvedev <omdv@protonmail.com>
2024-08-07 22:14:14 -0500

    docker aliases

--- home/om/features/cli/fish.nix
+++ home/om/features/cli/fish.nix
@@ -14,6 +14,8 @@
   hasFd = hasPackage "fd";
   hasKubecolor = hasPackage "kubecolor";
   hasGopass = hasPackage "gopass";
+  hasDocker = config.virtualisation.docker.enable;
+  hasDockerCompose = hasPackage "docker-compose";
 
   hasBat = config.programs.bat.enable;
   hasNnn = config.programs.nnn.enable;
@@ -52,7 +54,8 @@ in {
       hm = "home-manager --flake .";
       hms = "home-manager --flake . switch ";
       snrs = "sudo nixos-rebuild --flake . switch";
-      dps = "docker ps -a --format 'table {{.Names}}\t{{.Image}}\t{{.Ports}}\t{{.Status}}'";
+      dps = mkIf hasDocker "docker ps -a --format 'table {{.Names}}\t{{.Image}}\t{{.Ports}}\t{{.Status}}'";
+      dc = mkIf hasDockerCompose "docker-compose";
     };
 
     shellAliases = {
