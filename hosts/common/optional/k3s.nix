ecc12017ec17e3930ad36b80fe99e06663ad6ed6
Oleg Medvedev <omdv@protonmail.com>
2024-08-17 18:39:22 -0500

    k3s and network IP

--- hosts/common/optional/k3s.nix
+++ hosts/common/optional/k3s.nix
@@ -5,10 +5,14 @@
 
   services.k3s = {
     enable = true;
-    extraArgs = ''
-      --no-deploy traefik
-      --no-deploy metrics-server
-      --flannel-backend=vxlan
-    '';
+    role = "server";
+    extraArgs = toString [
+      "--disable traefik"
+      "--disable metrics-server"
+      "--flannel-backend=vxlan"
+    ];
   };
+
+  # open the k3s ports
+  networking.firewall.allowedTCPPorts = [ 6443 ];
 }
--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -17,16 +17,20 @@
     hostId = "c6589261";
     hostName = "homelab";
     networkmanager.enable = true;
+    interfaces = {
+      enp1s0 = {
+        useDHCP = false;
+        ipv4.addresses = [ {
+          address = "192.168.1.100";
+          prefixLength = 24;
+        } ];
+      };
+    };
   };
 
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    # kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
-    # binfmt.emulatedSystems = [
-      # "aarch64-linux"
-      # "i686-linux"
-    # ];
   };
 
   system.stateVersion = "23.05";
