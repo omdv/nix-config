1c59fa34295838fc5b500c8c31ee91bf52cddbe7
Oleg Medvedev <omdv@protonmail.com>
2024-08-17 10:41:25 -0500

    homelab hardware config

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   inputs,
   ...
 }: {
@@ -19,14 +18,12 @@
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
-    binfmt.emulatedSystems = [
-      "aarch64-linux"
-      "i686-linux"
-    ];
+    # kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
+    # binfmt.emulatedSystems = [
+      # "aarch64-linux"
+      # "i686-linux"
+    # ];
   };
-
-  hardware.opengl.enable = true;
 
   system.stateVersion = "23.05";
 }
