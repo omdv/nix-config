c675b1e3336ca2a379b7de18dae6a40ecc756fd2
Oleg Medvedev <omdv@protonmail.com>
2024-08-18 12:12:19 -0500

    rearrange networking

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -16,24 +16,17 @@
   networking = {
     hostId = "c6589261";
     hostName = "homelab";
-    networkmanager.enable = true;
+    useDHCP = true;
     interfaces = {
       enp2s0 = {
-        useDHCP = false;
+        useDHCP = true;
         ipv4.addresses = [ {
           address = "192.168.1.100";
           prefixLength = 24;
         } ];
       };
     };
-    defaultGateway = "192.168.1.1";
-    nameservers = [ "8.8.8.8" "8.8.4.4" ];
   };
-
-  # web and plex
-  networking.firewall.allowedTCPPorts = [
-    80 443 32400
-  ];
 
   boot = {
     loader.systemd-boot.enable = true;
