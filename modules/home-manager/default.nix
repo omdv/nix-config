d981f502967ab0d3d21455c236db785202ef9aa5
Oleg Medvedev <omdv@protonmail.com>
2024-08-18 11:49:54 -0500

    remove shellcolor module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,7 +2,6 @@
   fonts = import ./fonts.nix;
   monitors = import ./monitors.nix;
   pass-secret-service = import ./pass-secret-service.nix;
-  shellcolor = import ./shellcolor.nix;
   wallpaper = import ./wallpaper.nix;
   colors = import ./colors.nix;
   i3scaling = import ./i3scaling.nix;
