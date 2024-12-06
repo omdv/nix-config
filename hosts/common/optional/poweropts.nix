647e90db22afe5f5c73bdaf0e375cec2cd7597a2
Oleg Medvedev <omdv@protonmail.com>
2024-12-06 16:59:04 -0600

    power settings

--- hosts/common/optional/poweropts.nix
+++ hosts/common/optional/poweropts.nix
@@ -1,15 +1,32 @@
-{ inputs, ... }: {
+{ pkgs, lib, ... }: {
 
-  imports = [inputs.auto-cpufreq.nixosModules.default];
 
-  programs.auto-cpufreq = {
+  powerManagement.enable = true;
+
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=yes
+    AllowHibernation=yes
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=yes
+    HibernateDelaySec=1h
+    HandleLidSwitch=suspend-then-hibernate
+    HandleLidSwitchExternalPower=ignore
+    IdleAction=hibernate
+    IdleActionSec=30min
+  '';
+
+  # # glibc is needed for auto-cpufreq
+  # environment.systemPackages = with pkgs; [
+  #   glibc
+  # ];
+
+  services.auto-cpufreq = {
     enable = true;
     settings = {
       charger = {
         governor = "performance";
         turbo = "auto";
       };
-
       battery = {
         governor = "powersave";
         turbo = "auto";
