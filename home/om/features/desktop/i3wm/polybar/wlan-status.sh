454b0aaa3a499bf674fc2ddd16e3a14085de5f8c
Oleg Medvedev <omdv@protonmail.com>
2024-12-15 21:12:37 -0600

    minor icon and color changes

--- home/om/features/desktop/i3wm/polybar/wlan-status.sh
+++ home/om/features/desktop/i3wm/polybar/wlan-status.sh
@@ -1,32 +1,9 @@
-# Copyright (C) 2014 Alexander Keller <github@nycroth.com>
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#------------------------------------------------------------------------
 INTERFACE=$(iw dev | awk '$1=="Interface"{print $2}')
-#------------------------------------------------------------------------
 
 COLOR_GE80="$1"
 COLOR_GE60="$2"
 COLOR_GE40="$3"
 COLOR_LOWR="$4"
-
-# COLOR_GE80=${COLOR_GE80:-#00FF00}
-# COLOR_GE60=${COLOR_GE60:-#FFF600}
-# COLOR_GE40=${COLOR_GE40:-#FFAE00}
-# COLOR_LOWR=${COLOR_LOWR:-#FF0000}
-# COLOR_DOWN=${COLOR_LOWR}
 
 # As per #36 -- It is transparent: e.g. if the machine has no battery or wireless
 # connection (think desktop), the corresponding block should not be displayed.
@@ -34,7 +11,7 @@ COLOR_LOWR="$4"
 
 # If the wifi interface exists but no connection is active, "down" shall be displayed.
 if [[ "$(cat /sys/class/net/"$INTERFACE"/operstate)" = 'down' ]]; then
-    echo "%{F$COLOR_LOWR}down%{F-}"
+    echo "%{F$COLOR_LOWR}󰖪 {F-}"
     exit
 fi
 
@@ -47,14 +24,14 @@ QUALITY=$(iw dev "${INTERFACE}" link | grep 'dBm$' | grep -Eoe '-[0-9]{2}' | awk
 #------------------------------------------------------------------------
 
 # color
-if [[ $QUALITY -ge 80 ]]; then
+if [[ $QUALITY -ge 75 ]]; then
     COLOR=$COLOR_GE80
-elif [[ $QUALITY -ge 60 ]]; then
+elif [[ $QUALITY -ge 50 ]]; then
     COLOR=$COLOR_GE60
-elif [[ $QUALITY -ge 40 ]]; then
+elif [[ $QUALITY -ge 30 ]]; then
     COLOR=$COLOR_GE40
 else
     COLOR=$COLOR_LOWR
 fi
 
-echo "%{F$COLOR}WLAN $QUALITY% %{F-}"
+echo "%{F$COLOR}󰖩 $QUALITY% %{F-}"
