b4a09dc02efe0462d20c0e8a46044320b01e8f15
Oleg Medvedev <omdv@protonmail.com>
2024-12-15 18:31:22 -0600

    ssh folder mode

--- home/om/features/cli/ssh.nix
+++ home/om/features/cli/ssh.nix
@@ -6,6 +6,11 @@ in {
   };
 
   home.activation = {
+    sshDirectory = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p ~/.ssh
+      chmod 700 ~/.ssh
+    '';
+
     generateSSHKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       if [ ! -f ~/.ssh/id_rsa ]; then
         /run/current-system/sw/bin/ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "" -C "${config.home.username}@${hostName}";
