82fd21b7aebdbfa1bae313bef46a295ada606c58
Oleg Medvedev <omdv@protonmail.com>
2025-02-09 11:36:11 -0600

    k3s with tailscale support

--- hosts/common/optional/k3s.nix
+++ hosts/common/optional/k3s.nix
@@ -3,22 +3,38 @@
     pkgs.k3s
   ];
 
+  # add k3s user and group
+  users.groups.k3s = {};
+  users.users.k3s = {
+    group = "k3s";
+    uid = 1100;
+  };
+
+  # enable k3s service with tailscale support
+  # use --flannel-backend=vxlan for conventional k3s networking
   services.k3s = {
     enable = true;
     role = "server";
+    package = pkgs.symlinkJoin {
+      name = "k3s-with-deps";
+      paths = [ pkgs.k3s pkgs.tailscale ];
+    };
     extraFlags = toString [
       "--disable traefik"
       "--disable metrics-server"
-      # "--flannel-backend=vxlan"
       "--tls-san=100.105.86.80" # tailscale
       "--tls-san=192.168.1.98"  # lan
       "--tls-san=127.0.0.1"
       "--vpn-auth-file=/run/user-secrets/k3s_tailscale_auth"
       "--node-external-ip=100.105.86.80"
     ];
+    environmentFile = pkgs.writeText "k3s-environment" ''
+      PATH=${pkgs.tailscale}/bin:$PATH
+    '';
   };
 
   # open the k3s ports for the cluster
+  # TODO: check if qbittorrent is needed
   networking.firewall.allowedTCPPorts = [
     80
     443
