54399bbba9b50429d4ab31b8c7103605e0d2883f
Oleg Medvedev <omdv@protonmail.com>
2025-02-14 20:21:42 -0600

    correct iscsi settings

--- hosts/common/optional/iscsi.nix
+++ hosts/common/optional/iscsi.nix
@@ -1,9 +1,21 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    open-iscsi
+    openiscsi
   ];
   services.openiscsi = {
     enable = true;
     name = "iqn.2025-02.rs.x9:homelab";
   };
+
+  # Bind mount nsenter to /usr/bin for k3s longhorn compatibility
+  system.activationScripts.nsenter = ''
+    mkdir -p /usr/bin
+    ln -sfn ${pkgs.util-linux}/bin/nsenter /usr/bin/nsenter
+  '';
+
+  # Bind mount iscsiadm to /usr/bin for k3s longhorn compatibility
+  system.activationScripts.iscsiadm = ''
+    mkdir -p /usr/bin
+    ln -sfn ${pkgs.openiscsi}/bin/iscsiadm /usr/bin/iscsiadm
+  '';
 }
