6c09e2c9e3fed25efd27aea94057d815a4a44c35
Oleg Medvedev <omdv@protonmail.com>
2025-02-15 15:23:37 -0600

    zfs mount and reenable iscsi

--- hosts/common/optional/zfs.nix
+++ hosts/common/optional/zfs.nix
@@ -3,9 +3,14 @@
     zfs
   ];
 
-
   services.zfs = {
     autoScrub.enable = true;
     autoSnapshot.enable = false;
+  };
+
+  # mount longhorn storage pool to zfs
+  fileSystems."/var/lib/longhorn" = {
+    device = "pool/longhorn";
+    fsType = "zfs";
   };
 }
