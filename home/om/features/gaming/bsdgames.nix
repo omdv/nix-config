14f1a4e63d44859e588542f8a32155e3537f2f4a
Oleg Medvedev <omdv@protonmail.com>
2025-02-16 08:19:50 -0600

    remove zfs mounting

--- home/om/features/gaming/bsdgames.nix
+++ home/om/features/gaming/bsdgames.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    bsdgames
+home.packages = with pkgs; [
+  fish
+  (pkgs.runCommand "bsd-games-renamed" {} ''
+    mkdir -p $out/bin $out/share
+    cp -r ${pkgs.bsdgames}/bin/* $out/bin/
+    cp -r ${pkgs.bsdgames}/share/* $out/share/
+    mv $out/bin/fish $out/bin/bsd-fish
+    '')
   ];
 }
