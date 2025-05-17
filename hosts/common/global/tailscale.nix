741b7933d316f3cf5ef48677d46b62ce20b97fc1
Oleg Medvedev <omdv@protonmail.com>
2025-05-16 21:02:29 -0500

    tailscale settings

--- hosts/common/global/tailscale.nix
+++ hosts/common/global/tailscale.nix
@@ -1,13 +1,10 @@
-{lib, ...}: {
+{
   services.tailscale = {
     enable = true;
     port = 41414;
-    useRoutingFeatures = lib.mkDefault "client";
-    # extraUpFlags = [
-    #   "--login-server tailscale.${config.networking.domain}"
-    #   ];
+    useRoutingFeatures = "both";
+    extraUpFlags = [ "--accept-dns=false" ];
   };
 
-  # firewall punching
   networking.firewall.allowedUDPPorts = [41414];
 }
