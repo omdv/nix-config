21371f5190e6022d47534fd5bd7a0c1578b9610d
Oleg Medvedev <omdv@protonmail.com>
2025-05-17 11:20:09 -0500

    current networking

--- hosts/framework/networking.nix
+++ hosts/framework/networking.nix
@@ -29,35 +29,51 @@ in {
 
   networking = {
     hostName = "framework";
-    networkmanager.enable = true;
     useNetworkd = false;
+    networkmanager = {
+      enable = true;
+      dns = lib.mkForce "none";
+      dispatcherScripts = [
+        {
+          source = mkScript {
+            deps = [ pkgs.coreutils pkgs.systemd pkgs.networkmanager ];
+            script = ''
+              IFACE="$1"
+              STATE="$2"
+
+              echo "Dispatcher fired: $1 $2 at $(date)" >> /tmp/nm-dispatcher.log
+
+              if [ "$STATE" != "up" ]; then
+                exit 0
+              fi
+
+              if [ "$STATE" = "up" ]; then
+                echo "Ignoring DHCP-provided DNS on $IFACE" >> /tmp/nm-dispatcher.log
+                nmcli device modify "$IFACE" ipv4.ignore-auto-dns yes
+                nmcli device modify "$IFACE" ipv6.ignore-auto-dns yes
+              fi
+
+              case "$IFACE" in
+                wg0-mullvad)
+                  echo "Mullvad interface is up." >> /tmp/nm-dispatcher.log
+                  resolvectl dns "$IFACE" 193.138.218.74 185.213.154.45
+                  resolvectl domain "$IFACE" "~."
+                  resolvectl status "$IFACE" >> /tmp/nm-dispatcher.log
+                  ;;
+                tailscale0)
+                  echo "Tailscale interface is up." >> /tmp/nm-dispatcher.log
+                  resolvectl dns "$IFACE" 100.100.100.100
+                  resolvectl domain "$IFACE" "ts.net"
+                  resolvectl status "$IFACE" >> /tmp/nm-dispatcher.log
+                  ;;
+                *)
+                  echo "Unknown interface: $IFACE" >> /tmp/nm-dispatcher.log
+                  ;;
+              esac
+            '';
+          };
+        }
+      ];
+    };
   };
-
-  # Configure NetworkManager to use specific DNS for VPN connections
-  networking.networkmanager.dispatcherScripts = [
-    {
-      source = mkScript {
-        deps = [ pkgs.coreutils pkgs.systemd ];
-        script = ''
-IFACE="$1"
-STATUS="$2"
-
-if [[ "$STATUS" != "up" ]]; then
-  exit 0
-fi
-
-case "$IFACE" in
-  wg0-mullvad)
-    resolvectl dns "$IFACE" 193.138.218.74 185.213.154.45
-    resolvectl domain "$IFACE" "~."
-    ;;
-  tailscale0)
-    resolvectl dns "$IFACE" 100.100.100.100
-    resolvectl domain "$IFACE" "ts.net"
-    ;;
-esac
-        '';
-      };
-    }
-];
 }
