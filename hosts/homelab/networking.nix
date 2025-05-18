b712c3379c05fe973cbe30dfbfadd4b3703be4c9
Oleg Medvedev <omdv@protonmail.com>
2025-05-17 21:45:08 -0500

    homelab networking

--- hosts/homelab/networking.nix
+++ hosts/homelab/networking.nix
@@ -2,15 +2,36 @@
   networking = {
     hostId = "c6589261";
     hostName = "homelab";
-    useDHCP = true;
+    useNetworkd = true;
     interfaces = {
       enp2s0 = {
-        useDHCP = true;
+        useDHCP = false;
         ipv4.addresses = [{
           address = "192.168.1.98";
           prefixLength = 24;
         }];
       };
     };
+    defaultGateway = "192.168.1.1";
+    nameservers = [
+      "192.168.1.1"
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
+  };
+
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+
+  services.resolved = {
+    enable = true;
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+    ];
+    domains = [ "~." ];
+    extraConfig = ''
+      DNSStubListener=yes
+    '';
   };
 }
