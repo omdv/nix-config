20d29d0047fcdb123f33c77a5063f0c8d2e4f1e7
Oleg Medvedev <omdv@protonmail.com>
2025-10-17 21:12:53 -0500

    add: devenv unstable

--- home/om/features/productivity/default.nix
+++ home/om/features/productivity/default.nix
@@ -12,6 +12,7 @@
   home.packages = [
     pkgs.beancount # ledger alternative
     pkgs.bruno # api tool
+    pkgs.unstable.devenv # dev environment manager
     pkgs.pgcli # great postgres cli from Ukraine
     pkgs.visidata # cli for data analysis
     pkgs-unstable.claude-code # up-to-date claude code
