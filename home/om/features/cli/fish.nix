80cf22146cb5eb5b2ba75add312da6f3d8e44049
Oleg Medvedev <omdv@protonmail.com>
2025-10-17 21:11:41 -0500

    add: add support for shell gpt shortcuts

--- home/om/features/cli/fish.nix
+++ home/om/features/cli/fish.nix
@@ -109,6 +109,33 @@ in {
             rm -- $NNN_TMPFILE
         end
       '';
+      # sgpt integration
+      _sgpt_commandline = ''
+        # Get the current command line content
+        set -l _sgpt_prompt (commandline)
+
+        # Only proceed if there is a prompt
+        if test -z "$_sgpt_prompt"
+            return
+        end
+
+        # Append an hourglass to the current command
+        commandline -a "⌛"
+        commandline -f end-of-line  # needed to display the icon
+
+        # Get the output of the sgpt command
+        set -l _sgpt_output (echo "$_sgpt_prompt" | sgpt --shell --no-interaction)
+
+        if test $status -eq 0
+            # Replace the command line with the output from sgpt
+            commandline -r -- (string trim "$_sgpt_output")
+            commandline -a "  # $_sgpt_prompt"
+        else
+            # If the sgpt command failed, remove the hourglass
+            commandline -f backward-delete-char
+            commandline -a "  # ERROR: sgpt command failed"
+        end
+      '';
     };
     interactiveShellInit = ''
       # XDG configs
@@ -160,6 +187,9 @@ in {
       set -gx fzf_fd_opts --hidden --no-ignore --exclude=.git --max-depth 5
       set -gx fzf_preview_dir_cmd eza --all --color=always
       set -gx fzf_diff_highlighter diff-so-fancy
+
+      # sgpt integration
+      bind \cs _sgpt_commandline
     '';
   };
 }
