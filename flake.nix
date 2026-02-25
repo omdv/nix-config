0efe163060195c7f61bcfae358eb373d35464a2d
Oleg Medvedev <omdv@protonmail.com>
2026-02-24 22:46:23 -0600

    remove: unused hydra jobs

--- flake.nix
+++ flake.nix
@@ -82,7 +82,6 @@
       homeManagerModules = import ./modules/home-manager;
 
       overlays = import ./overlays { inherit inputs outputs; };
-      hydraJobs = import ./hydra.nix { inherit inputs outputs; };
 
       packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
       devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
