bf7e3fd88daa8a17522e151def7693d6316eb96d
Oleg Medvedev <omdv@protonmail.com>
2026-03-07 20:47:40 -0600

    home/cli: add btop alongside htop

---
+++ home/om/features/cli/btop.nix
@@ -0,0 +1,34 @@
+{ ... }: {
+  programs.btop = {
+    enable = true;
+    settings = {
+      # Appearance
+      color_theme          = "Default";
+      rounded_corners      = true;
+
+      # Behaviour
+      vim_keys             = true;
+      update_ms            = 1000;
+
+      # CPU
+      show_coretemp        = true;
+      temp_scale           = "celsius";
+      cpu_single_graph     = false;
+      cpu_sensor           = "Auto";
+
+      # Processes
+      proc_tree            = true;
+      proc_sort            = "cpu lazy";
+      proc_reversed        = true;
+      proc_per_core        = true;
+
+      # Memory
+      mem_graphs           = true;
+      show_swap            = true;
+
+      # Misc
+      base_10_sizes        = false;
+      clock_format         = "%H:%M";
+    };
+  };
+}
