4fa16cc29a299f434c196f49ef3823a9d21a4ad4
Oleg Medvedev <omdv@protonmail.com>
2026-03-16 20:34:36 -0500

    add zram, fix btrfs, remove ollama

--- hosts/common/optional/btrfs.nix
+++ hosts/common/optional/btrfs.nix
@@ -1,7 +1,12 @@
-{
+{ lib, ... }: {
   services.btrfs.autoScrub = {
     enable = true;
-    interval = "weekly";
+    interval = "monthly";
     fileSystems = [ "/" ];
+  };
+
+  # Prevent scrub from firing at boot when the window was missed
+  systemd.timers."btrfs-scrub--" = {
+    timerConfig.Persistent = lib.mkForce false;
   };
 }
