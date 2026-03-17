68d05281c39f7a0ad0ef82662183892567265e4c
Oleg Medvedev <omdv@protonmail.com>
2026-03-16 21:00:17 -0500

    net: remove avahi, use resolved for mDNS; fix mbsync network ordering

--- hosts/common/optional/printers.nix
+++ hosts/common/optional/printers.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }:
 {
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
   services.printing = {
     enable = true;
     drivers = [ pkgs.brlaser ];
