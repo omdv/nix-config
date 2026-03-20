df0b43fc8d865b3731f4953bd9fb4b4136fd4338
Oleg Medvedev <omdv@protonmail.com>
2026-03-20 17:46:52 -0500

    nvim: remove lead listchar, replaced by indent-blankline

--- home/om/features/nixvim/settings.nix
+++ home/om/features/nixvim/settings.nix
@@ -100,7 +100,7 @@
 
       # Enable chars list
       list = true; # Show invisible characters (tabs, eol, ...)
-      listchars = "eol:↲,tab:|->,lead:·,trail:•,extends:→,precedes:←,nbsp:␣";
+      listchars = "eol:↲,tab:|->,trail:•,extends:→,precedes:←,nbsp:␣";
 
       # More space in the neovim command line for displaying messages
       cmdheight = 2;
