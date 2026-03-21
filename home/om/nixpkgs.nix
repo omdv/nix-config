75dc295937045f31c26c35190bc10ba67b95696c
Oleg Medvedev <omdv@protonmail.com>
2026-03-20 23:03:03 -0500

    cleanup unused code

--- home/om/nixpkgs.nix
+++ home/om/nixpkgs.nix
@@ -3,17 +3,6 @@
   outputs,
   ...
 }: {
-  # nix = {
-  #   settings = {
-  #     experimental-features = [
-  #       "nix-command"
-  #       "flakes"
-  #       "ca-derivations"
-  #     ];
-  #     warn-dirty = false;
-  #   };
-  # };
-
   nixpkgs = {
     overlays = builtins.attrValues outputs.overlays;
     config = {
