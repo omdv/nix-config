d6c0509a34c76e99c0f59851026a3fbd7d3b1d6c
Oleg Medvedev <omdv@protonmail.com>
2026-04-05 19:21:34 -0500

    productivity tools cleanup, deployment scripts

--- home/om/features/desktop/gnome/default.nix
+++ home/om/features/desktop/gnome/default.nix
@@ -1,5 +1,3 @@
-{config, ...}: {
-  # Scaling in GNOME
-  dconf.settings."org/gnome/desktop/interface".text-scaling-factor = 1.3333;
-  dconf.settings."org/gnome/desktop/background".picture-uri-dark = config.wallpaper.url;
+{
+  dconf.settings."org/gnome/desktop/interface".text-scaling-factor = 1.0;
 }
