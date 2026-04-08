979b849bd920b4c4395330afbb1363288c67e6f0
Oleg Medvedev <omdv@protonmail.com>
2026-04-08 18:28:47 -0500

    install mirage-proxy as systemd service

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,4 +4,5 @@
   pi-coding-agent = pkgs.callPackage ./pi-coding-agent {};
   pi-dynamic-context-pruning = pkgs.callPackage ./pi-dynamic-context-pruning {};
   taskplane = pkgs.callPackage ./taskplane {};
+  mirage-proxy = pkgs.callPackage ./mirage-proxy {};
 }
